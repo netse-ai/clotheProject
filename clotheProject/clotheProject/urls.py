de5852f42c16d7b13a1375cf47383db4c965d0bb
tear727 <tear728@gmail.com>
2016-03-07 19:50:56 -0800

    user app implemented

--- clotheProject/clotheProject/urls.py
+++ clotheProject/clotheProject/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.conf.urls.static import static
+
 from rest_framework import routers, serializers, viewsets
 
 from items.models import Item
@@ -19,4 +21,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- clotheProject/users/models.py
+++ clotheProject/users/models.py
@@ -8,7 +8,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, null=True)
     favorites = models.ForeignKey(Item, blank=True, null=True)
     # comments = models.ForeignKey('comments.Comment', related_name="user_profile_comments", null=True)
-    photo = models.ImageField(upload_to='media/', blank=True, null=True)
+    photo = models.ImageField(upload_to='media/', blank=True)
     likes = models.IntegerField(blank=True)
     dislikes = models.IntegerField(blank=True)
 
