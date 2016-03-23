a91e18917a192b1a5b62370c2a2396cb6d7c4573
tear727 <tear728@gmail.com>
2016-03-23 15:33:33 -0700

    UserSerializer PUT test

--- clotheProject/api/serializers.py
+++ clotheProject/api/serializers.py
@@ -27,5 +27,6 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     def create(self, validated_data):
         userprofile_data = validated_data.pop('userprofile')
         user = User.objects.create(**validated_data)
-        UserProfile.objects.create(user=user, **userprofile_data)
+        for user_data in userprofile_data:
+            UserProfile.objects.create(user=user, **userprofile_data)
         return user
