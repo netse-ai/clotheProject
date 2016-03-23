7e0927f6b9c1baf1e967981d79588de42859201b
tear727 <tear728@gmail.com>
2016-03-23 15:18:23 -0700

    read_only=True added to serializer

--- clotheProject/api/serializers.py
+++ clotheProject/api/serializers.py
@@ -19,7 +19,7 @@ class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    userprofile = UserProfileSerializer()
+    userprofile = UserProfileSerializer(read_only=True)
     class Meta:
         model = User
         fields = ('username', 'url', 'email', 'is_staff', 'password', 'userprofile')
