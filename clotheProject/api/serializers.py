e29247d087bf1a8b11e953042d7af720f57b6d12
tear727 <tear728@gmail.com>
2016-03-25 11:24:00 -0700

    favorites table/api serializer

--- clotheProject/api/serializers.py
+++ clotheProject/api/serializers.py
@@ -45,8 +45,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         user_profile = UserProfile.objects.create(user=user, **profile_data)
         favorite = Favorite()
         favorite.save()
-        for item in favorites_data:
-            favorite.items.add(item)
+        favorite.items.add(favorites_data)
+        favorite.save()
         return user
 
     # def update(self, instance, validated_data):
