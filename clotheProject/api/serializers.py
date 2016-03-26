add132cbf954aa76c22c876042e880d332a2abe1
tear727 <tear728@gmail.com>
2016-03-25 17:10:23 -0700

    favorites table/api serializer

--- clotheProject/api/serializers.py
+++ clotheProject/api/serializers.py
@@ -15,7 +15,7 @@ class ItemSerializer(serializers.HyperlinkedModelSerializer):
 class FavoriteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Favorite
-        # exclude = ('id', 'user')
+        fields =('id',)
 
 
 class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
