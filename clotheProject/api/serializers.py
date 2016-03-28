e517bdd5e5452d6cc5c91799a780de649154e8a2
tear727 <tear728@gmail.com>
2016-03-28 14:02:57 -0700

    Revert "issue with updating users via api"
    This reverts commit 231d003addaeec9fc17679cf321cef1ff8c1cba6.

--- clotheProject/api/serializers.py
+++ clotheProject/api/serializers.py
@@ -41,8 +41,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         # item_data = favorites_data.pop('items')
         # print item_data
         user = User.objects.create_user(**validated_data)
-        for data in profile_data:
-            UserProfile.objects.create(user=user, **data)
+        user_profile = UserProfile.objects.create(user=user, **profile_data)
         # favorite = Favorite(user=user)
         # favorite.save()
         # print favorite.items
