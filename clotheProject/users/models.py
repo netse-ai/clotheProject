739ff4f612e8daa688fbb029158068a509e6899b
tear727 <tear728@gmail.com>
2016-04-02 16:27:43 -0700

    users

--- clotheProject/users/models.py
+++ clotheProject/users/models.py
@@ -20,9 +20,9 @@ class UserProfile(models.Model):
 #     pass
 #     userprofile = models.OneToOneField(UserProfile, null=True)
 #     item = models.ManyToManyField(Item)
-# 
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         UserProfile.objects.create(user=instance)
 #
-# post_save.connect(create_user_profile, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
