46ff07c23b6633fa26c1b7c1c9617354f9625839
tear727 <tear728@gmail.com>
2016-04-03 21:21:12 -0700

    added favorite btn functionality

--- clotheProject/items/urls.py
+++ clotheProject/items/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
 from items.models import Item
-from items.views import item_view
+from items.views import item_view, favorite_item
 
 urlpatterns = [
     url(r'^$', item_view, name="item_view"),
+    url(r'^favorites', favorite_item, name="favorite_item"),
     ]
--- clotheProject/items/views.py
+++ clotheProject/items/views.py
@@ -4,12 +4,31 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.edit import UpdateView
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
 
-from items.models import Item
+from items.models import Item, Favorite
 
 
 def item_view(request):
     items = Item.objects.all()
+    context = {"items":items}
     template = 'items/items.html'
-    context = {'items': items}
     return render(request, template, context)
+
+
+@login_required
+def favorite_item(request):
+    fav, created = Favorite.objects.get_or_create(user=request.user)
+    if request.method == "POST":
+        id = request.POST['id']
+        item = Item.objects.get(id=id)
+        item = Item.objects.get(id=id)
+        fav.items.add(item)
+        fav.save()
+        print fav
+    return HttpResponse('hello')
+    # if item_id:
+    #     fav = Favorite.objects.get(user=request.user)
+    #     if fav:
+    #         fav.save()
+    #         fav.items.add(item=int(item_id))
