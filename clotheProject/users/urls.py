5f2688e993611f8c811eca16d128e52731380e41
tear727 <tear728@gmail.com>
2016-04-05 16:51:37 -0700

    added urls

--- clotheProject/users/urls.py
+++ clotheProject/users/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
 from users.models import UserProfile
-from users.views import users_view, user_login, user_profile, user_logout
+from users.views import users_view, user_login, user_profile, user_logout, unfavorite_item
 
 urlpatterns = [
     url(r'^$', users_view, name="users_view"),
     url(r'^login', user_login, name="login"),
     url(r'^profile', user_profile, name="profile"),
     url(r'logout/$', user_logout, name="logout"),
+    url(r'^unfavorite', unfavorite_item, name="unfavorite_item"),
     ]
