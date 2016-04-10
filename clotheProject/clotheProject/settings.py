c9ad56621ae8e970d6fce8c91cd7ebb6546b7416
tear727 <tear728@gmail.com>
2016-04-10 11:50:33 -0700

    /opt/myenv/static/

--- clotheProject/clotheProject/settings.py
+++ clotheProject/clotheProject/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'opt/myenv/static/'
+STATIC_ROOT = '/opt/myenv/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = "/opt/myenv/media"
