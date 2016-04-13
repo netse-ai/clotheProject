a89654f35fbe942f6d4e7999a2b4b751d7b7fb97
tear727 <tear728@gmail.com>
2016-04-13 15:53:09 -0700

    save() userprofile breaking things?

--- clotheProject/users/models.py
+++ clotheProject/users/models.py
@@ -16,16 +16,16 @@ class UserProfile(models.Model):
     def __unicode__(self):
         return unicode(self.user.username)
 
-    def save(self, *args, **kwargs):
-        if self.photo:
-            size = (400, 400)
-            if not self.id and not self.photo:
-                return
-
-            super(UserProfile, self).save(*args, **kwargs)
-            image = Image.open(self.photo)
-            image = image.resize(size, Image.ANTIALIAS)
-            image.save(self.photo.path)
+    # def save(self, *args, **kwargs):
+    #     if self.photo:
+    #         size = (400, 400)
+    #         if not self.id and not self.photo:
+    #             return
+    #
+    #         super(UserProfile, self).save(*args, **kwargs)
+    #         image = Image.open(self.photo)
+    #         image = image.resize(size, Image.ANTIALIAS)
+    #         image.save(self.photo.path)
 #
 # class Favorites(models.Model):
 #     pass
