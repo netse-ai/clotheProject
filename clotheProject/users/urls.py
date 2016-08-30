59d618df7d2eb20df529f340f7769d0a044814ff
tear727 <tear728@gmail.com>
2016-08-30 15:13:44 -0700

    html changes and ursers url deleteD

--- clotheProject/users/urls.py
+++ clotheProject/users/urls.py
@@ -4,7 +4,7 @@ from users.models import UserProfile
 from users.views import users_view, user_login, user_profile, user_logout, unfavorite_item
 
 urlpatterns = [
-    url(r'^$', users_view, name="users_view"),
+    # url(r'^$', users_view, name="users_view"),
     url(r'^login', user_login, name="login"),
     url(r'^profile', user_profile, name="profile"),
     url(r'logout/$', user_logout, name="logout"),
